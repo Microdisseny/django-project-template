609e3534df105e9f2103787ebf012a152a63a326
Manel Clos <manelclos@gmail.com>
2018-09-25 12:43:34 +0200

    fix manage.py runserver
    closes Microdisseny/django-project-template/issues/13

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,16 +16,17 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.http import HttpResponse, HttpResponseForbidden
-import oauth2_provider.views as oauth2_views
+# import oauth2_provider.views as oauth2_views
 
 # OAuth2 provider endpoints
-oauth2_endpoint_views = [
-    url(r'^authorize/$', oauth2_views.AuthorizationView.as_view(),
-        name="authorize"),
-    url(r'^token/$', oauth2_views.TokenView.as_view(), name="token"),
-    url(r'^revoke-token/$', oauth2_views.RevokeTokenView.as_view(),
-        name="revoke-token"),
-]
+# uncomment for using some urls only
+# oauth2_endpoint_views = [
+#     url(r'^authorize/$', oauth2_views.AuthorizationView.as_view(),
+#         name="authorize"),
+#     url(r'^token/$', oauth2_views.TokenView.as_view(), name="token"),
+#     url(r'^revoke-token/$', oauth2_views.RevokeTokenView.as_view(),
+#         name="revoke-token"),
+# ]
 
 
 def is_authenticated(request):

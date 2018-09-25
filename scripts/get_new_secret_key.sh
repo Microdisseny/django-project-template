5a46cf4d2cb2319b4c53106e653487782cd6eac3
Martí Angelats i Ribera <ma@microdisseny.com>
2018-09-25 15:09:51 +0200

    Add utility script to generate secret keys
    Move scripts to a new folder and upgrade them
    
    closes #9

---
+++ scripts/get_new_secret_key.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())"
--- scripts/run_coverage.sh
+++ scripts/run_coverage.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+SCRIPTDIR=`dirname "$BASH_SOURCE"`
+MANAGE="$SCRIPTDIR/../manage.py"
+
 export ENVIRONMENT_NAME=test
 coverage run \
   --omit='manage.py','*migrations*','django_project/wsgi.py','django_project/settings-devel.py','django_project/settings-test.py' \
-  --source=. manage.py test tests \
+  --source=. $MANAGE test tests \
   && coverage report -m --skip-covered

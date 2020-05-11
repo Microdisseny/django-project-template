31a633a70b97b3e7ca06f6a867cfac0cc3301726
Alexandre <abusquets@gmail.com>
2020-05-11 11:07:46 +0200

    Add CustomAdminSite

--- django_project/admin.py
+++ django_project/admin.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.contrib import admin
 
-admin.site.site_header = settings.ADMIN_SITE_HEADER
-admin.site.site_title = settings.ADMIN_SITE_TITLE
-admin.site.index_title = settings.ADMIN_INDEX_TITLE
+from .views import dashboard_test
+
+class CustomAdminSite(admin.AdminSite):
+    site_header = settings.ADMIN_SITE_HEADER
+    site_title = settings.ADMIN_SITE_TITLE
+    index_title = settings.ADMIN_INDEX_TITLE

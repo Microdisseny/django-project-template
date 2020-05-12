62de59de58c26e64bb7d0410986f4ac89aa50ca0
Manel Clos <manelclos@gmail.com>
2020-05-12 09:54:52 +0200

    use postgresql database by default

--- django_project/settings.py
+++ django_project/settings.py
@@ -94,12 +94,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
-        # 'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
         # 'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        # 'NAME': os.environ.get('DB_NAME', ''),
+        'NAME': os.environ.get('DB_NAME', ''),
         'USER': os.environ.get('DB_USER', ''),
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': os.environ.get('DB_HOST', 'localhost'),

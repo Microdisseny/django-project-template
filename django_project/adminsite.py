7f33663cc01961f4d37c408a3d72e36dcef76ea3
Ainhoa Vázquez Zarza <ainhoa-97-12@live.com>
2021-07-30 09:11:05 +0200

    Millora SITE_URL

--- django_project/adminsite.py
+++ django_project/adminsite.py
@@ -10,6 +10,13 @@ class CustomAdminSite(admin.AdminSite):
     site_header = settings.ADMIN_SITE_HEADER
     site_title = settings.ADMIN_SITE_TITLE
     index_title = settings.ADMIN_INDEX_TITLE
+    site_url = settings.SITE_URL
+
+    def each_context(self, request):
+        context = super().each_context(request)
+        context['DOCUMENTATION_URL'] = settings.DOCUMENTATION_URL
+        context['DOCUMENTATION_TITLE'] = settings.DOCUMENTATION_TITLE
+        return context
 
     # def get_urls(self):
     #     urls = super().get_urls()
